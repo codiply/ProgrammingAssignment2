936510ce2f1dc8d90dec33759efaea84ae90355e
Panos <codiply@gmail.com>
2015-03-10 22:43:30 +0000

    Implemented makeCacheMatrix and cacheSolve. Added documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix.
+##
+## Example:
+##   n <- 1000
+##   mat <- matrix(data = rnorm(n*n), nrow = n, ncol = n)
+##   cachedMat <- makeCacheMatrix(mat)
+##   inv <- cacheSolve(cachedMat)
+##   inv <- cacheSolve(cachedMat) ## Call it a second time
+##   sum((inv %*% mat-diag(n))^2) ## This should be zero
 
-## Write a short comment describing this function
-
+#' Creates a container object for a matrix and its inverse.
+#'
+#' The set and get methods store and retrieve the underlying matrix.
+#' The setsolve and getsolve methods store and retrieve its inverse matrix.
+#' @param x The underlying matrix.
+#' @return A list of functions for setting and getting the matrix and its inverse.
+#' #' @examples
+#' mat <- matrix(data = rnorm(100), nrow = 10, ncol = 10); makeCacheMatrix(mat)
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(sol) s <<- sol
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+#' Caches and returns the inverse of a matrix.
+#'
+#' @param x A wrapped matrix using the function makeCacheMatrix.
+#' @return The inverse matrix.
+#' #' @examples
+#' cachedMatrix <- makeCacheMatrix(mat); inv <- cacheSolve(cachedMatrix)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+            message("getting cached data")
+            return(s)
+        }
+        mat <- x$get()
+        s <- solve(mat, ...)
+        x$setsolve(s)
+        s
 }
